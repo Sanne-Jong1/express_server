a72f14ef813828d9dc546c81f5a4b10e2dd0afb9
angel-0399 <ashrez0399@gmail.com>
2025-11-23 02:19:36 +0800

    add initial server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.get("/getData", async (req, res) => {
+  try {
+    const response = await axios.get("https://api.parts-catalogs.com/v1/catalogs/nissan/schemas?carId=c302e0c82d01db8fef520aeac7b92bd0&branchId=547&page=0");
+
+    
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server running"));
